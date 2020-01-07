068f212a9058e8078097ea027e43b3dce2ab18ed
Kerry Shetline <kshetline@campsystems.com>
2020-01-07 11:28:55 -0500

    Added SAM local invoke testing.

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      build and deploy this stack to your default AWS account/region
-* `cdk deploy-only` deploy this stack to your default AWS account/region without first doing a build
+* `npm run build`       compile typescript to js
+* `npm run watch`       watch for changes and compile
+* `npm run test`        perform the jest unit tests
+* `npm run test-local`  perform SAM invoke local testing
+* `cdk deploy`          build and deploy this stack to your default AWS account/region
+* `cdk deploy-only`     deploy this stack to your default AWS account/region without first doing a build
---
+++ sam-test/sam-test.js
@@ -0,0 +1,154 @@
+const fs = require('fs');
+const chalk = require('chalk');
+const { spawnSync } = require('child_process');
+const { isMatch } = require('lodash');
+
+const CHECK_MARK = '\u2714';
+const FAIL_MARK = '\u2718';
+
+const testEventInfo = {
+  getTimezone: [
+    {
+      testName: "should get correct timezone for Anchorage",
+      queryStringParameters: {
+        city: "anchorage, ak"
+      },
+      expectedResult: [{city: 'Anchorage, AK', zone: 'America/Anchorage' }]
+    },
+    {
+      testName: "should get correct timezone for Marseille",
+      queryStringParameters: {
+        city: "Marseille, FRA"
+      },
+      expectedResult: [{zone: 'Europe/Paris' }] // Note: comparison to full result not required
+    }
+  ],
+  reverse: [
+    {
+      testName: "should reverse string",
+      queryStringParameters: {
+        s: "foo"
+      },
+      expectedResult: 'oof'
+    },
+    {
+      testName: "should reverse string with function to test result",
+      queryStringParameters: {
+        s: "bar"
+      },
+      expectedResult: body => body === 'rab'
+    }
+  ]
+};
+
+let lines;
+
+try {
+  lines = fs.readFileSync('./template.yaml', { encoding: 'utf-8' }).split(/\r\n|\n|\r/);
+} catch (err) {
+  console.error('template.yaml not found');
+  process.exit(1);
+}
+
+let lastLine = '';
+let testCount = 0;
+let successCount = 0;
+
+lines.forEach(line => {
+  if (/^\s*Type\: AWS\:\:Lambda\:\:Function\s*$/.test(line)) {
+    const $ = /\s*(\w*?)([0-9A-Z]*):\s*$/.exec(lastLine);
+
+    if ($) {
+      const name = $[1];
+      const fullName = name + $[2];
+      const events = testEventInfo[name];
+
+      if (events && events.length > 0) {
+        console.log(chalk.blue(name), chalk.gray('(' + fullName + ')'));
+
+        events.forEach((eventInfo, index) => {
+          ++testCount;
+
+          const testName = eventInfo.testName;
+          const expectedStatus = eventInfo.expectedStatus || 200;
+          const expectedResult = eventInfo.expectedResult;
+          const template = {
+            body: `{"message": "${name}"}`,
+            resource: "/{proxy+}",
+            path: `/${name}`,
+            httpMethod: "GET",
+            isBase64Encoded: false
+          };
+
+          process.stdout.write(`  ${index + 1}: ${testName} `);
+
+          delete eventInfo.testName;
+          delete eventInfo.expectedStatus;
+          delete eventInfo.expectedResult;
+          Object.assign(template, eventInfo);
+
+          fs.writeFileSync('sam-test/event.json', JSON.stringify(template, null, 2), { encoding: 'utf-8' });
+          const results = spawnSync('sam', [
+            'local',
+            'invoke',
+            '-e',
+            '"sam-test/event.json"',
+            fullName
+          ], { encoding: 'utf-8', shell: true });
+
+          if (results.stderr && results.stderr.startsWith('Traceback ')) {
+            console.log(chalk.red(FAIL_MARK));
+            console.error(chalk.red('    Test failed: ' + results.stderr));
+          }
+          else {
+            let value;
+
+            try {
+              value = JSON.parse(results.stdout);
+
+              if (value && value.statusCode != null && value.body) {
+                value.body = JSON.parse(value.body);
+
+                if (value.statusCode === expectedStatus && testSucceeds(expectedResult, value.body)) {
+                  ++successCount;
+                  console.log(chalk.green(CHECK_MARK));
+                }
+                else {
+                  console.log(chalk.red(FAIL_MARK));
+                }
+              }
+              else {
+                console.log(chalk.red(FAIL_MARK));
+                console.error(chalk.red(`    Malformed response: ${results.stdout}\n    ${err}`));
+              }
+            }
+            catch (err) {
+              console.log(chalk.red(FAIL_MARK));
+              console.error(chalk.red(`    Malformed response: ${results.stdout}\n    ${err}`));
+            }
+          }
+        });
+      }
+    }
+  }
+
+  try {
+    fs.unlinkSync('sam-test/event.json');
+  }
+  catch (err) {}
+
+  lastLine = line;
+});
+
+console.log(`\nTest count: ${testCount}` +
+  (successCount > 0 ? ', ' + chalk.green(`succeeded: ${successCount}`) : '') +
+  (testCount - successCount > 0 ? ', ' + chalk.red(`failed: ${testCount - successCount}`) : ''));
+
+function testSucceeds(expectedResult, body) {
+  if (typeof expectedResult === 'function') {
+    return expectedResult(body);
+  }
+  else {
+    return isMatch(body, expectedResult);
+  }
+}
